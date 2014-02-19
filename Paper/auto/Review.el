3b0df2eef0493c9b6d395dcdd6c4c016b324b524
Abraham Xiao <q289196573@gmail.com>
2014-02-20 01:09:26 +0400

    Yesterday once more. You are no longer in China! Be a real postgraduate student or you will get doomed. Trust me, Valve and Dota2 is the best solution. But not for now. You will feel the pleasure sooner or later in The International 4.

--- Paper/auto/Review.el
+++ Paper/auto/Review.el
@@ -1,7 +1,9 @@
 (TeX-add-style-hook "Review"
  (lambda ()
     (LaTeX-add-labels
-     "sec:paper77")
+     "sec:paper77"
+     "sec:paper97"
+     "sec:paper99")
     (TeX-run-style-hooks
      "latex2e"
      "art10"
